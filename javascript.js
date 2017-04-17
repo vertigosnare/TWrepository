88e00d264c9d2088f52e221ca450101c87a4fb01
Caitlin Ahern <u3142796@uni.canberra.edu.au>
2017-04-17 22:57:28 +1000

    Loading Page
    (accidentally reverted the changes) Html for loading page including
    images and text.
    
    Javascript that hides the div after 5 seconds and shows the next div
    (on boarding screens) after 5.5 seconds (when the loading page
    disappears)
    
    initial CSS for loading page

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+
+
+// Loading Screen Caitlin
+
+// hiding loading div
+setTimeout (function() {
+	$(".hiding").fadeOut("slow"); 
+}, 5000); // <-- time in milliseconds
+	  
+
+	  
+// 	showing user onboarding screen using fadeIn
+$(function(){
+
+	$(".showingnextscreen").hide(); // hide div before it can fade in 
+  
+	setTimeout (function() {
+		$(".showingnextscreen").fadeIn("slow"); 
+	}, 5500);
+	
+});
