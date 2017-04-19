9d8d733f4dcd3937b78b06d501207714186fe1c6
stephaniebrink <sj.brink@gmail.com>
2017-04-19 19:38:12 +1000

    Onboarding screens
    Launch screen and my onboarding screens are working together and
    responsive

--- javascript.js
+++ javascript.js
@@ -1,22 +1,63 @@
-// JavaScript Document
+$(document).ready(function() {
+    if ($('.wrong-device').css('display') === 'block') {
+        $('body').css('background-color', '#7f143c'); // uses plain redwine background for wrong device message
+    }
+    
+    if ($('.launch').css('display') === 'block') {
+        $('body').addClass('redwine-gradient'); // uses redwine gradient background for launch screen
+    }
+});
 
 
-// Loading Screen Caitlin
+// LAUNCH SCREEN (Caitlin)
 
-// hiding loading div
+// hide launch screen after 5 seconds
 setTimeout (function() {
-	$(".hiding").fadeOut("slow"); 
+	$(".launch").fadeOut("slow"); 
 }, 5000); // <-- time in milliseconds
 	  
 
-	  
-// 	showing user onboarding screen using fadeIn
+// fade in user onboarding screen after 5.3 seconds
 $(function(){
+    if ($('.launch').css('display') === 'block') { // only if the launch screen is displayed - don't want it to fade in if wrong device message is displayed
+        setTimeout (function() {
+            $("#onboarding1").fadeIn("slow");
+            $('body').addClass('gold-gradient'); // use gold gradient background for onboarding screens
+        }, 5300);
+    }
+});
 
-	$(".showingnextscreen").hide(); // hide div before it can fade in 
-  
-	setTimeout (function() {
-		$(".showingnextscreen").fadeIn("slow"); 
-	}, 5500);
-	
+
+// USER ONBOARDING SCREENS (Steph)
+
+// 1st user onboarding screen, moving to 2nd user onboarding screen
+$('#first-next').on('click touchstart', function() {
+    $('#onboarding1').css('display', 'none');
+    $('#onboarding2').css('display', 'block');
+});
+
+// swiping left is also an acceptable way to navigate
+$('#onboarding1').on('swipeleft', function(){
+    this.hide('slide', {direction: 'left'}, 1000);
+    $('#onboarding2').show('slide', {direction: 'right'}, 1000);
+});
+
+
+// 2nd user onboarding screen, moving to 3rd user onboarding screen
+$('#second-next').on('click touchstart', function() {
+    $('#onboarding2').css('display', 'none');
+    $('#onboarding3').css('display', 'block');
+});
+
+// 3rd user onboarding screen, moving to 4th (and final) user onboarding screen
+$('#third-next').on('click touchstart', function() {
+    $('#onboarding3').css('display', 'none');
+    $('#onboarding4').css('display', 'block');
+});
+
+// when the begin button is selected, the last onboarding screen fades out
+$('#begin').on('click touchstart', function() {
+    $('#onboarding4').fadeOut();
+    $('body').removeClass('redwine-gradient');
+    $('body').removeClass('gold-gradient');
 });
